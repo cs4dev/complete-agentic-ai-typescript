dd1ebd52877c2ad169e17d900ce8e4208de7e889
cs4dev <cs4dev@gmail.com>
2025-08-16 15:31:38 +0800

    1. openai example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Happy developing ✨')
+import { OpenAI } from "openai";
+import * as dotenv from "dotenv";
+import { ChatCompletionMessageParam } from "openai/resources";
+
+dotenv.config();
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+(async () => {
+  const messages: Array<ChatCompletionMessageParam> = [
+    { role: "user", content: "What is 2+2?" }
+  ];
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages
+  });
+  console.log(response.choices[0].message.content);
+})();
